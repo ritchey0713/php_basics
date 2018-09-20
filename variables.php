9d24cad5601b24af636cee87e4cee98a0b7e7c9f
ritchey0713 <ritchey0713@gmail.com>
2018-09-20 13:22:43 -0400

    add file for variables

--- variables.php
+++ variables.php
@@ -7,10 +7,7 @@
   <body>
 
     <?php
-      echo("<h1> Hello World </h1>");
-      echo "<hr />";
-      echo("<p> Hey there! </p>");
-      echo("<ul> Goodbye! </ul>");
+    
     ?>
 
   </body>
