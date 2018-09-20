eb88338ad3275bb064e89b1a9b458d863d2b4b97
ritchey0713 <ritchey0713@gmail.com>
2018-09-20 12:01:45 -0400

    add more echo lines

---
+++ site.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+      echo("Hello World");
+      echo("Hey there!");
+      echo("Goodbye!");
+    ?>
+
+  </body>
+</html>
