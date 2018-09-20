e27b4bf616072533fef5534fcb3298659aab1f64
ritchey0713 <ritchey0713@gmail.com>
2018-09-20 14:43:30 -0400

    add variables

--- variables.php
+++ variables.php
@@ -7,7 +7,12 @@
   <body>
 
     <?php
-    
+    $characterName = "John";
+    $characterAge = "35";
+    echo "There was once a man named $characterName <br />";
+    echo "He was $characterAge years old <br />";
+    echo "He really liked the name $characterName  <br />";
+    echo "But he didnt like being $characterAge <br />";
     ?>
 
   </body>
