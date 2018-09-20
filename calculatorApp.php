c3da562c7c133008eefa802f2759537b3dd3d2f2
ritchey0713 <ritchey0713@gmail.com>
2018-09-20 16:36:08 -0400

    add function set

--- calculatorApp.php
+++ calculatorApp.php
@@ -6,9 +6,14 @@
   </head>
   <body>
 
-    <?php
+    <form action="calculatorApp.php" method="get">
+      <input type="number" name="num1" />
+      <br />
+      <input type="number" name="num2" />
+      <input type="submit"  />
+    </form>
 
-    ?>
+    Answer: <?php echo $_GET["num1"] + $_GET["num2"] ?>
 
   </body>
 </html>
