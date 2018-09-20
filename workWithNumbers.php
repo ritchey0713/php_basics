3ff622afa2d23a761ffc098d3589b1aaec6d150a
ritchey0713 <ritchey0713@gmail.com>
2018-09-20 15:49:03 -0400

    number fN

---
+++ workWithNumbers.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo abs(-100);
+      echo " ";
+      echo pow(2,4);
+      echo " ";
+      echo sqrt(144);
+      echo " ";
+      echo max(2, 10);
+      echo " ";
+      echo min(2, 10);
+      echo " ";
+      echo round(3.7);
+      echo " ";
+      echo ceil(10.1);
+      echo " ";
+      echo floor(9.9);
+    ?>
+  </body>
+</html>
