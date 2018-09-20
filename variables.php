905fae8c69f5eef82654064b86adb42ed56b1ff6
ritchey0713 <ritchey0713@gmail.com>
2018-09-20 14:50:09 -0400

    change age to int

--- variables.php
+++ variables.php
@@ -8,7 +8,7 @@
 
     <?php
     $characterName = "John";
-    $characterAge = "35";
+    $characterAge = 35;
     echo "There was once a man named $characterName <br />";
     echo "He was $characterAge years old <br />";
     $characterName = "Mike";
