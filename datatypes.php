2034ad27f37dc09989c3b685ff795ecb3f57bbba
ritchey0713 <ritchey0713@gmail.com>
2018-09-20 15:08:04 -0400

    add datatypes

---
+++ datatypes.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+      $phrase = "Hello";
+      $int = 50;
+      $floating = 20.5;
+      $boolean = true;
+      $noValue = null;
+    ?>
+
+  </body>
+</html>
