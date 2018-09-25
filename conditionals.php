fda65abee932df1fdb2187226953e12889c1698c
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-09-25 11:35:40 -0400

    use negation operator

---
+++ conditionals.php
@@ -0,0 +1,38 @@
+<?php 
+  $a = 5;
+  $b= 3;
+
+  // if (($a == $b) == false){
+  //   echo "values are not equal!";
+  // }
+
+  // if ($a <> $b){
+  //   echo "values are not equal!";
+  // }
+
+  if ($a != $b ){
+    echo "Values are not equal";
+  }
+
+
+  /*if ($a == $b){
+    echo 'values are equal';
+  } elseif( $a < $b){
+    echo '$a < $b';
+  } elseif($a > $b) {
+    echo '$a > $b';
+  }else {
+    echo 'values are not equal';
+  }*/
+
+/*if ($score >= 60) {
+  echo "You completed the level!";
+} elseif ($score <= 30){
+ echo "You will need tgo practice!";
+}else {
+  echo "please try again!";
+}*/
+
+
+
+?>
