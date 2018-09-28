fe334262277415360ca17890d9653cf08fce1a47
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-09-27 22:37:50 -0400

    add logics

--- conditionals.php
+++ conditionals.php
@@ -10,9 +10,11 @@
   //   echo "values are not equal!";
   // }
 
-  if ($a != $b ){
-    echo "Values are not equal";
-  }
+  // if ($a != $b ){
+  //   echo "Values are not equal";
+  // }
+
+   
 
 
   /*if ($a == $b){
@@ -32,6 +34,31 @@
 }else {
   echo "please try again!";
 }*/
+
+// $num = 1000000;
+
+// if ($num >= 10){
+//   if ($num <= 1000) {
+//     echo "Your number is within the range!";
+//   } else {
+//     echo "your number is too high!";
+//   }
+// } else {
+//   echo "your number is not in range!";
+// }
+
+
+$num = 100;
+
+// if ($num >= 10 && $num <= 1000) {
+//     echo "Your number is within the range!";
+//   } else {
+//     echo "your number is NOT in range!";
+//   }
+
+$var1 = true && false;
+
+$var2 = true and false;
 
 
 
